388e80bffcf1fa358bf7d2d0edc7d5114cb9b200
AayushNerurkar <aayushnerurkar@eng.rizvi.edu.in>
2024-10-17 08:44:16 +0530

    Add files via upload

---
+++ Experiment 1/Experiment 1 Task 1.c
@@ -0,0 +1,18 @@
+/*Name:Aayush Nerurkar Class:FE UIN:241A036
+Aim: WAP to print basic details on screen and
+format it using escape sequence.*/
+#include <stdio.h>
+int main()
+{
+    printf("Full Name\t:Aayush\tNerurkar");
+    printf("\nAge\t\t:17");
+    printf("\nClass\t\t:FE");
+    printf("\nDivision\t:A2");
+    printf("\nUIN\t\t:241A036");
+    return 0;
+}
+/* Full Name    :Aayush Nerurkar
+Age             :17
+Class           :FE
+Division        :A2
+UIN             :241A036 */
