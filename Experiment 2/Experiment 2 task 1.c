448106daeb7187782c38d75c7ed3ea0ca4e8ac00
AayushNerurkar <aayushnerurkar@eng.rizvi.edu.in>
2024-11-05 08:22:42 +0530

    Add files via upload

---
+++ Experiment 2/Experiment 2 task 1.c
@@ -0,0 +1,21 @@
+/*Aim: WAP to find if entered number is even or odd.
+Name:Aayush Nerurkar
+Class:FE
+UIN:241A036
+*/
+#include <stdio.h>
+int main()
+{
+int a;
+printf("\t\t\t\t\t\t***EVEN ODD FINDER***");
+printf("\n\n\n\t\t\t\t\t\tEnter your number:");
+scanf("%d",&a);
+if(a%2==0)
+   {printf("\t\t\t\t\t\t     %d is even",a);
+    }
+else
+   {printf("\t\t\t\t\t\t     %d is odd",a);
+   }
+ return 0;
+ }
+/* Output:*/
